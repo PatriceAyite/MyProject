8c587debf2dbf51635459719748815d904e33630
Patrice Ayite <patricayite72@gmail.com>
2024-10-21 12:13:52 +0000

    To do App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
+import { NgClass, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+
+export interface TodoItem {
+  id:number;
+  task:string;
+  completed:boolean
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor,NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-todo';
+  
+  todoLIst : TodoItem [] = [];
+  newTask:string =''
+
+  addTask():void {
+      if(this.newTask.trim() !== ''){
+        const newTodoItem : TodoItem  = {
+          id : Date.now(),
+          task: this.newTask,
+          completed:false
+        }
+
+
+        this.todoLIst.push(newTodoItem)
+        this.newTask = '';
+      }
+  }
+
+
+  toggleompleted(index:number):void {
+    this.todoLIst[index].completed = !this.todoLIst[index].completed
+
+  }
+
+  deleteTask(id:number):void {
+    this.todoLIst = this.todoLIst.filter(item => item.id !== id)
+    
+  }
 }
